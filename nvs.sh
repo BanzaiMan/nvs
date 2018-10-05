806338aaa98d0ffe3f0ffb7b70e1bcc3d0594c8d
Josh Kalderimis <josh.kalderimis@gmail.com>
2018-10-05 15:54:08 -0700

    add some more install debug information

--- nvs.sh
+++ nvs.sh
@@ -74,6 +74,11 @@ nvs() {
 		else
 			curl -L -# "${NODE_URI}" -o "${NODE_ARCHIVE}"
 		fi
+		
+		if [ ! -f "${NODE_ARCHIVE}" ]; then
+			echo "Failed to download bootstrap node binary."
+			return 1
+		fi
 
 		if [ "${NVS_OS}" = "win" ]; then
 			"${NVS_ROOT}/tools/7-Zip/7zr.exe" e "-o${NVS_HOME}/cache" -y "${NODE_ARCHIVE}" "${NODE_FULLNAME}/${NODE_EXE}" > /dev/null 2>&1
@@ -84,7 +89,7 @@ nvs() {
 		fi
 
 		if [ ! -f "${NODE_PATH}" ]; then
-			echo "Failed to download bootstrap node binary."
+			echo "Failed to download setup node binary."
 			return 1
 		fi
 		echo ""
